b7c809a41c859d09ed322f8ed2db0694d39de531
PalomoRodriguezHectorLenin <hpalomor2000@alumno.ipn.mx>
2022-11-09 10:09:01 -0600

    logica

---
+++ public/js/logica.js
@@ -0,0 +1,28 @@
+var socket = io('');
+
+let message = document.getElementById('messege');
+let username = document.getElementById('username');
+let btn = document.getElementById('send');
+let output = document.getElementById('output');
+let actions = document.getElementById('actions');
+
+btn.addEventListener('click', function(){
+    socket.emit('chat:message', {
+        message: message.value,
+        username: username.value
+    })
+});
+
+message.addEventListener('keypress', function(){
+    socket.emit('chat:typing', username.value);
+});
+
+socket.on('chat:message', function(data){
+    actions.innerHTML = '';
+    output.innerHTML += `<p><strong>${data.username}</strong>: ${data.message}</p>`
+
+});
+
+socket.on('chat:typing', function(data){
+    actions.innerHTML= `<p><en>${data} está escribiendo...</en></p>`
+});
