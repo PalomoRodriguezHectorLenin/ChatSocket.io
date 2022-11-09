8959d01d3c4c6cc11c317a6dd4df70f5e2ca6a9d
PalomoRodriguezHectorLenin <hpalomor2000@alumno.ipn.mx>
2022-11-09 10:14:43 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+var server = require('http').createServer(app);
+
+app.set(port, process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+server.listen(app.get('port'), ()=>{
+    console.log('server on port', app.get('port'));
+});
+
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+io.on('connection', (socket)=>{
+    console.log('nueva conexion', socket.id);
+
+    socket.on('chat:message', (data)=>{
+        io.socket.emit('chat:message', data);
+    });
+    socket.on('chat:typing', (data)=>{
+        socket.broadcast.emit('chat:typing', data);
+    });
+});
